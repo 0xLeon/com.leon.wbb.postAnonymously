a1c523f2c0fad200802d6703c06def2b970b6b60
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-11-13 22:57:40 +0100

    normalized line endings

--- files/lib/system/event/listener/ThreadAddFormPostAnonymouslyListener.class.php
+++ files/lib/system/event/listener/ThreadAddFormPostAnonymouslyListener.class.php
@@ -1,1 +1,43 @@
-<?php// wcf importsrequire_once(WCF_DIR.'lib/system/event/EventListener.class.php');/** * Removes user specific data from posts and shows corresponding option *  * @author	Stefan Hahn * @copyright	2012 Stefan Hahn * @license	Simplified BSD License License <http://projects.swallow-all-lies.com/licenses/simplified-bsd-license.txt> * @package	com.leon.wbb.postAnonymously * @subpackage	system.event.listener * @category 	Burning Board */class ThreadAddFormPostAnonymouslyListener implements EventListener {	protected static $userID = 0;	protected static $ipAddress = '';		/**	 * @see EventListener::execute()	 */	public function execute($eventObj, $className, $eventName) {		if ($eventObj->board->getPermission('canPostAnonymously')) {			if ($eventName === 'show') {				WCF::getTPL()->append('additionalSettings', WCF::getTPL()->fetch('messageFormSettingsPostAnonymously'));			}			else if ($eventName === 'save') {				if (isset($_POST['postAnonymously']) && intval($_POST['postAnonymously'])) {					self::$userID = WCF::getUser()->userID;					self::$ipAddress = WCF::getSession()->ipAddress;										$eventObj->username = WCF::getLanguage()->get('wbb.threadAdd.anonymousUsername');					WCF::getUser()->userID = 0;					WCF::getSession()->ipAddress = '';				}			}			else if ($eventName === 'saved') {				WCF::getUser()->userID = self::$userID;				WCF::getSession()->ipAddress = self::$ipAddress;			}		}	}}
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
+
+/**
+ * Removes user specific data from posts and shows corresponding option
+ * 
+ * @author	Stefan Hahn
+ * @copyright	2012 Stefan Hahn
+ * @license	Simplified BSD License License <http://projects.swallow-all-lies.com/licenses/simplified-bsd-license.txt>
+ * @package	com.leon.wbb.postAnonymously
+ * @subpackage	system.event.listener
+ * @category 	Burning Board
+ */
+class ThreadAddFormPostAnonymouslyListener implements EventListener {
+	protected static $userID = 0;
+	protected static $ipAddress = '';
+	
+	/**
+	 * @see EventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		if ($eventObj->board->getPermission('canPostAnonymously')) {
+			if ($eventName === 'show') {
+				WCF::getTPL()->append('additionalSettings', WCF::getTPL()->fetch('messageFormSettingsPostAnonymously'));
+			}
+			else if ($eventName === 'save') {
+				if (isset($_POST['postAnonymously']) && intval($_POST['postAnonymously'])) {
+					self::$userID = WCF::getUser()->userID;
+					self::$ipAddress = WCF::getSession()->ipAddress;
+					
+					$eventObj->username = WCF::getLanguage()->get('wbb.threadAdd.anonymousUsername');
+					WCF::getUser()->userID = 0;
+					WCF::getSession()->ipAddress = '';
+				}
+			}
+			else if ($eventName === 'saved') {
+				WCF::getUser()->userID = self::$userID;
+				WCF::getSession()->ipAddress = self::$ipAddress;
+			}
+		}
+	}
+}
